5ef9fd4caba05ed353cce90e9d99c909d5eee2cb
seondays <agseon9@gmail.com>
2024-05-16 17:51:21 +0900

    feat: 열린 이슈, 닫힌 이슈의 정보를 DB에서 가져올 수 있도록 custom메서드 추가

--- be/src/main/java/issuetracker/be/repository/IssueRepository.java
+++ be/src/main/java/issuetracker/be/repository/IssueRepository.java
@@ -1,7 +1,12 @@
 package issuetracker.be.repository;
 
 import issuetracker.be.domain.Issue;
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+
+  List<Issue> findByIsOpenIsTrue();
+
+  List<Issue> findByIsOpenIsFalse();
 }
