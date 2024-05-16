5c939b8afa553d7356c827dadcbbea7effdf880e
정채영 <sarah100217@gmail.com>
2024-05-16 11:41:52 +0900

    feat: 테이블 상단 렌더링

--- fe/src/pages/IssueListPage.tsx
+++ fe/src/pages/IssueListPage.tsx
@@ -6,6 +6,8 @@ import dropdownIcon from "../assets/dropdownIcon.svg";
 import searchIcon from "../assets/search.svg";
 import labelIcon from "../assets/label.svg";
 import milestoneIcon from "../assets/milestone.svg";
+import alertIcon from "../assets/alertCircle.svg";
+import archiveIcon from "../assets/archive.svg";
 
 export default function IssueListPage() {
   const { currentUser } = useCurrentUser();
@@ -38,6 +40,35 @@ export default function IssueListPage() {
             <IssueCreationButton>+ 이슈 작성</IssueCreationButton>
           </ButtonsWrapper>
         </TapAndFilterWrapper>
+        <IssueTableTop>
+          <IssueCheckBox type="checkbox" name="wholeIssue" />
+          <TableContent>
+            <SelectOpenAndClosedIssueBox>
+              <OpenedIssueTap>
+                <img src={alertIcon} alt="opened icon" />
+                <span>열린 이슈(2)</span>
+              </OpenedIssueTap>
+              <ClosedIssueTap>
+                <img src={archiveIcon} alt="closed icon" />
+                <span>닫힌 이슈(2)</span>
+              </ClosedIssueTap>
+            </SelectOpenAndClosedIssueBox>
+            <FilterBtnsOnTable>
+              <TableFilterBtn>
+                담당자 <img src={dropdownIcon} alt="dropdown icon" />
+              </TableFilterBtn>
+              <TableFilterBtn>
+                레이블 <img src={dropdownIcon} alt="dropdown icon" />
+              </TableFilterBtn>
+              <TableFilterBtn>
+                마일스톤 <img src={dropdownIcon} alt="dropdown icon" />
+              </TableFilterBtn>
+              <TableFilterBtn>
+                작성자 <img src={dropdownIcon} alt="dropdown icon" />
+              </TableFilterBtn>
+            </FilterBtnsOnTable>
+          </TableContent>
+        </IssueTableTop>
       </CommonS.Wrapper>
     </>
   );
@@ -62,7 +93,6 @@ const FilterBtn = styled(CommonS.SpaceBetween)`
   padding: 0 24px;
   align-items: center;
   width: 128px;
-  box-sizing: border-box;
   align-items: center;
   border-right: 1px solid rgba(217, 219, 233, 1);
 
@@ -95,7 +125,7 @@ const FilterSearchBox = styled.div`
 
 const ButtonsWrapper = styled(CommonS.SpaceBetween)`
   align-items: center;
-  width: 456px;
+  width: 465px;
   height: 100%;
 `;
 
@@ -137,4 +167,59 @@ const IssueCreationButton = styled.button`
   color: white;
   border: none;
   border-radius: 10px;
+`;
+
+const IssueTableTop = styled.div`
+  display: flex;
+  height: 64px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  border: 1px solid rgba(217, 219, 233, 1);
+  margin-top: 25px;
+  padding: 0 32px;
+`;
+
+const SelectOpenAndClosedIssueBox = styled(CommonS.SpaceBetween)`
+  align-items: center;
+  width: 227px;
+`;
+
+const OpenedIssueTap = styled(CommonS.SpaceBetween)`
+  width: 102px;
+  font-weight: 700;
+  font-size: 16px;
+  color: rgba(20, 20, 43, 1);
+`;
+
+const ClosedIssueTap = styled(CommonS.SpaceBetween)`
+  width: 102px;
+  font-weight: 500;
+  font-size: 16px;
+  color: rgba(78, 75, 102, 1);
+`;
+
+const FilterBtnsOnTable = styled(CommonS.SpaceBetween)`
+  align-items: center;
+  width: 416px;
+  height: 100%;
+`;
+
+const TableFilterBtn = styled(CommonS.SpaceBetween)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80px;
+  height: 100%;
+  color: rgba(78, 75, 102, 1);
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+const TableContent = styled(CommonS.SpaceBetween)`
+  margin-left: 32px;
+  width: 1200px;
+`;
+
+const IssueCheckBox = styled.input`
+  border: 1px solid rgba(217, 219, 233, 1);
 `;
