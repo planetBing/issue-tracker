08e653878f43057e1c90ac9f4f3eb631d2800699
seondays <agseon9@gmail.com>
2024-05-16 17:53:32 +0900

    refactor: http상태코드 일관성 있게 바꿈

--- be/src/main/java/issuetracker/be/controller/IssueController.java
+++ be/src/main/java/issuetracker/be/controller/IssueController.java
@@ -1,12 +1,14 @@
 package issuetracker.be.controller;
 
+import issuetracker.be.dto.IssueGetResponseDto;
 import issuetracker.be.dto.IssueSaveRequestDto;
 import issuetracker.be.service.IssueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +21,9 @@ public class IssueController {
     this.issueService = issueService;
   }
 
+  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("/issue")
-  public ResponseEntity<?> saveIssue(@RequestBody IssueSaveRequestDto issueSaveRequestDto) {
+  public void saveIssue(@RequestBody IssueSaveRequestDto issueSaveRequestDto) {
     issueService.save(issueSaveRequestDto);
   }
 
