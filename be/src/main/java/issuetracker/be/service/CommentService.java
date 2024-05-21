eab3f9e84e9e211f9ff5e9800d21b5258af16a1a
wontae <dnjsxo0616@gmail.com>
2024-05-21 10:29:10 +0900

    feat: CommentService에 코멘트 삭제 및 수정 기능 추가 #20

--- be/src/main/java/issuetracker/be/service/CommentService.java
+++ be/src/main/java/issuetracker/be/service/CommentService.java
@@ -2,9 +2,12 @@ package issuetracker.be.service;
 
 import issuetracker.be.domain.Comment;
 import issuetracker.be.dto.CommentSaveRequest;
+import issuetracker.be.dto.CommentUpdateRequest;
 import issuetracker.be.repository.CommentRepository;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +29,26 @@ public class CommentService {
 
     log.debug("저장된 코멘트 : {}", saveComment);
   }
+
+  public void deleteComment(Long comment_id) {
+    commentRepository.deleteById(comment_id);
+  }
+
+  public Comment updateComment(CommentUpdateRequest commentUpdateRequest) {
+    Long commentId = commentUpdateRequest.getComment_id();
+    String contents = commentUpdateRequest.getContents();
+    Optional<Comment> optionalComment = commentRepository.findById(commentId);
+
+    if (optionalComment.isPresent()) {
+      Comment comment = optionalComment.get();
+      comment.setContents(contents);
+      comment.setCreated_at(LocalDateTime.now());
+      return commentRepository.save(comment);
+    } else {
+      throw new NoSuchElementException("존재하지 않는 코멘트입니다.");
+    }
+  }
+
 
   public List<Comment> getComment(Long id) {
     return commentRepository.findByIssueId(id);
