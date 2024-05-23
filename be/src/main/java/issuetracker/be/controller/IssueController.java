d8bd5ebcaecb68bb94df84c9049a00a25304de87
wontae <dnjsxo0616@gmail.com>
2024-05-23 19:18:22 +0900

    feat: 이슈 컨트롤러에 이슈 상세 응답 기능 추가 #32

--- be/src/main/java/issuetracker/be/controller/IssueController.java
+++ be/src/main/java/issuetracker/be/controller/IssueController.java
@@ -1,11 +1,16 @@
 package issuetracker.be.controller;
 
+import issuetracker.be.dto.CommentResponse;
+import issuetracker.be.dto.IssueDetailResponse;
 import issuetracker.be.dto.IssueListResponse;
 import issuetracker.be.dto.IssueSaveRequest;
+import issuetracker.be.service.CommentService;
 import issuetracker.be.service.IssueService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,9 +21,12 @@ public class IssueController {
 
   private IssueService issueService;
 
+  private CommentService commentService;
+
   @Autowired
-  public IssueController(IssueService issueService) {
+  public IssueController(IssueService issueService, CommentService commentService) {
     this.issueService = issueService;
+    this.commentService = commentService;
   }
 
   @ResponseStatus(HttpStatus.CREATED)
@@ -32,5 +40,13 @@ public class IssueController {
   public IssueListResponse findAllIssue() {
     IssueListResponse result = issueService.getAllIssue();
     return result;
+  }
+
+  @GetMapping("issue/{issueId}")
+  public IssueDetailResponse issueDetail(@PathVariable Long issueId) {
+
+    List<CommentResponse> commentResponse = commentService.getCommentResponse(issueId);
+
+    return issueService.getDetailResponse(commentResponse, issueId);
   }
 }
