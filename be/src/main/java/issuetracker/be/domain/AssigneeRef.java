0c698ecdbe6e83dcb9bbab76caa808f5bed398fd
seondays <agseon9@gmail.com>
2024-05-23 18:13:44 +0900

    feat: 빈생성자 어노테이션 추가

--- be/src/main/java/issuetracker/be/domain/AssigneeRef.java
+++ be/src/main/java/issuetracker/be/domain/AssigneeRef.java
@@ -1,11 +1,13 @@
 package issuetracker.be.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("issue_assignee")
 @Getter
+@NoArgsConstructor
 public class AssigneeRef {
   @Id
   private String user_name;
--- be/src/main/java/issuetracker/be/domain/LabelRef.java
+++ be/src/main/java/issuetracker/be/domain/LabelRef.java
@@ -1,12 +1,17 @@
 package issuetracker.be.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
+@Setter
 @ToString
+@NoArgsConstructor
 @Table("issue_label")
 public class LabelRef {
   @Id
