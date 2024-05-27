416c5ec929713cb10eabfd0b4811d84b2bc5e71d
jonghyeok-97 <dkswhdgur1209@naver.com>
2024-05-27 23:31:21 +0900

    remove: Security 없이 OAuth를 구현하기 때문에 Config 파일 제거 #35

--- be/src/main/java/issuetracker/be/config/OAuth2Config.java
+++ be/src/main/java/issuetracker/be/config/OAuth2Config.java
@@ -1,35 +1,33 @@
 package issuetracker.be.config;
 
-import issuetracker.be.service.PrincipalOauthUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class OAuth2Config {
 
-  private final PrincipalOauthUserService principalOauthUserService;
-
-  @Autowired
-  public OAuth2Config(PrincipalOauthUserService principalOauthUserService) {
-    this.principalOauthUserService = principalOauthUserService;
-  }
-
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-    http.csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> {
-          auth.requestMatchers("/issue/**").authenticated();
-          auth.anyRequest().permitAll();
-        })
-        .oauth2Login(oauth2 ->
-            oauth2.defaultSuccessUrl("/label")
-                .userInfoEndpoint(userInfo ->
-                    userInfo.userService(principalOauthUserService)
-                )
-        );
-    return http.build();
-  }
-}
+//@Configuration
+//public class OAuth2Config {
+//
+//  private final PrincipalOauth2UserService principalOauth2UserService;
+//
+//  @Autowired
+//  public OAuth2Config(PrincipalOauth2UserService principalOauth2UserService) {
+//    this.principalOauth2UserService = principalOauth2UserService;
+//  }
+//
+//  @Bean
+//  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+//    http.csrf(csrf -> csrf.disable())
+//        .authorizeHttpRequests(auth -> {
+//          auth.requestMatchers("/issue/**").authenticated();
+//          auth.anyRequest().permitAll();
+//        })
+//        .oauth2Login(oauth2 ->
+//            oauth2.defaultSuccessUrl("/label")
+//                .userInfoEndpoint(userInfo ->
+//                    userInfo.userService(principalOauth2UserService)
+//                )
+//        );
+//    return http.build();
+//  }
+//}
