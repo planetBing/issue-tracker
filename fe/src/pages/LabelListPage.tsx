d1bdb240ec904d056425dee0f2ff333827db2a99
planetBing <sarah100217@gmail.com>
2024-05-27 10:33:46 +0900

    feat: 라벨 수정 기능

--- fe/src/pages/LabelListPage.tsx
+++ fe/src/pages/LabelListPage.tsx
@@ -5,18 +5,11 @@ import { useCurrentUser } from "../contexts/CurrentUserProvider";
 import * as CommonS from "../styles/common";
 import useApi from "../hooks/api/useApi";
 import LabelMilestoneTap from "../components/LabelMilestoneTap";
-import { Label } from "../Model/types";
+import { Label, LabelForm } from "../Model/types";
 import LabelComponent from "../components/Label";
 import greyEditIcon from "../assets/greyEdit.svg";
 import trashIcon from "../assets/trash.svg";
-import IconRefresh from "../assets/IconsRefresh.svg";
-
-interface LabelForm {
-  name: string;
-  background_color: string;
-  text_color: string;
-  description: null | string;
-}
+import LabelFormBox from "../components/LabelForm";
 
 const initialLabelForm = {
   name: "",
@@ -42,46 +35,74 @@ export default function LabelListPage() {
     refetch: refetchLabelList,
     postData: postNewLabel,
     deleteData: deleteLabel,
+    putData: updateLabel,
   } = useApi<Label[]>("/label");
   const [isCreationMode, setIsCreationMode] = useState<boolean>(false);
   const [labelForm, setLabelForm] = useState<LabelForm>(initialLabelForm);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [editLabelId, setEditLabelId] = useState<number | null>(null);
 
-  const handleInputLabel = (
-    e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLInputElement>
-  ) => {
-    const { name, value } = e.target;
-    setLabelForm((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+  const inputHandlers = {
+    handleInputLabel: (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setLabelForm((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    },
+    handleRandomBackgroundColor: () => {
+      setLabelForm((prev) => ({
+        ...prev,
+        background_color: getRandomHexColor(),
+      }));
+    },
+    handleTextColorChange: (e: React.ChangeEvent<HTMLSelectElement>) => {
+      const value = e.target.value;
+      setLabelForm((prev) => ({
+        ...prev,
+        text_color: value,
+      }));
+    },
   };
 
-  const handleRandomBackgroundColor = () => {
-    setLabelForm((prev) => ({
-      ...prev,
-      background_color: getRandomHexColor(),
-    }));
-  };
-
-  const handleTextColorChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value;
-    setLabelForm((prev) => ({
-      ...prev,
-      text_color: value,
-    }));
-  };
-
-  const handleAddLabel = async () => {
-    await postNewLabel("/label", labelForm);
-    setIsCreationMode(false);
-    refetchLabelList();
-  };
+  // const handleAddLabel = async () => {
+  //   await postNewLabel("/label", labelForm);
+  //   setIsCreationMode(false);
+  //   refetchLabelList();
+  // };
 
   const handleDeleteLabel = async (labelId: number) => {
     await deleteLabel(`/label/${labelId.toString()}`);
     refetchLabelList();
   };
 
+  const handleEditLabel = (label: Label) => {
+    const { id, ...labelWithoutId } = label;
+    setLabelForm(labelWithoutId);
+    setIsEditMode(true);
+    setEditLabelId(label.id);
+  };
+
+  const handleCancel = () => {
+    setIsCreationMode(false);
+    setIsEditMode(false);
+    setEditLabelId(null);
+    setLabelForm(initialLabelForm);
+  };
+
+  const handleSubmit = async () => {
+    if (isEditMode && editLabelId !== null) {
+      const labelFormWithId = { ...labelForm, id: editLabelId };
+      await updateLabel(`/label`, labelFormWithId);
+    } else {
+      await postNewLabel("/label", labelForm);
+    }
+    setIsCreationMode(false);
+    setIsEditMode(false);
+    setEditLabelId(null);
+    setLabelForm(initialLabelForm);
+    refetchLabelList();
+  };
   return (
     <>
       <PageHeader loggedInUserImageSrc={currentUser?.image_path} />
@@ -93,70 +114,12 @@ export default function LabelListPage() {
           </AddLabelBtn>
         </LabelPageHeader>
         {isCreationMode && (
-          <LabelCreationContainer>
-            <h3>새로운 레이블 추가</h3>
-            <CreateLabelInfoArea>
-              <LabelDesignShowBox>
-                <LabelDiv
-                  $backgroundColor={labelForm.background_color}
-                  $textColor={labelForm.text_color}
-                >
-                  {labelForm.name === "" ? "Label" : labelForm.name}
-                </LabelDiv>
-              </LabelDesignShowBox>
-              <CreateLabelTextArea>
-                <InputContainer $width={"100%"}>
-                  <LabelBox>
-                    <label>이름</label>
-                  </LabelBox>
-                  <input
-                    name="name"
-                    placeholder="레이블의 이름을 입력하세요"
-                    onChange={handleInputLabel}
-                  />
-                </InputContainer>
-                <InputContainer $width={"100%"}>
-                  <LabelBox>
-                    <label>설명(선택)</label>
-                  </LabelBox>
-                  <input
-                    name="description"
-                    placeholder="레이블에 대한 설명을 입력하세요"
-                    onChange={handleInputLabel}
-                  />
-                </InputContainer>
-                <ColorPickerBox>
-                  <InputContainer $width={"240px"}>
-                    <LabelBox>
-                      <label>배경 색상</label>
-                    </LabelBox>
-                    <input
-                      type="text"
-                      value={labelForm.background_color}
-                      readOnly
-                    />
-                    <img
-                      src={IconRefresh}
-                      alt="refresh"
-                      onClick={handleRandomBackgroundColor}
-                    />
-                  </InputContainer>
-                  <TextColorPicker onChange={handleTextColorChange}>
-                    <option value="white">밝은색</option>
-                    <option value="#6e7191">어두운 색</option>
-                  </TextColorPicker>
-                </ColorPickerBox>
-              </CreateLabelTextArea>
-            </CreateLabelInfoArea>
-            <CreateLabelButtonArea>
-              <CreateCancelButton onClick={() => setIsCreationMode(false)}>
-                X 취소
-              </CreateCancelButton>
-              <CreateDoneButton onClick={handleAddLabel}>
-                + 완료
-              </CreateDoneButton>
-            </CreateLabelButtonArea>
-          </LabelCreationContainer>
+          <LabelFormBox
+            labelObj={labelForm}
+            inputHandler={inputHandlers}
+            handleCancel={() => setIsCreationMode(false)}
+            handleSubmit={handleSubmit}
+          />
         )}
         <IssueTableTop>
           <LabelNum>{labeListData?.length}개의 레이블</LabelNum>
@@ -164,6 +127,17 @@ export default function LabelListPage() {
         {isLabelDataLoading && <p>...loading</p>}
         {labeListData?.map((labelObj) => {
           const { id, description } = labelObj;
+          if (isEditMode && editLabelId === id) {
+            return (
+              <LabelFormBox
+                key={`editLabel-${id}`}
+                labelObj={labelForm}
+                inputHandler={inputHandlers}
+                handleCancel={handleCancel}
+                handleSubmit={handleSubmit}
+              />
+            );
+          }
           return (
             <IssueTable key={`labelList-${id}`}>
               <LabelInfo>
@@ -173,7 +147,7 @@ export default function LabelListPage() {
                 <p>{description}</p>
               </LabelInfo>
               <TableButtonArea>
-                <LabelEditButton>
+                <LabelEditButton onClick={() => handleEditLabel(labelObj)}>
                   <img src={greyEditIcon} alt="edit icon" />
                   편집
                 </LabelEditButton>
@@ -269,104 +243,4 @@ const LabelDeleteButton = styled(CommonS.Center)`
   color: rgba(255, 59, 48, 1);
   font-size: 12px;
   cursor: pointer;
-`;
-
-const LabelCreationContainer = styled.section`
-  width: 100%;
-  height: 337px;
-  background-color: white;
-  margin-top: 25px;
-  border-radius: 16px;
-  padding: 32px;
-
-  & h3 {
-    margin-top: 0;
-    font-size: 20px;
-  }
-`;
-
-const CreateLabelInfoArea = styled(CommonS.SpaceBetween)`
-  width: 100%;
-  height: 153px;
-  margin-bottom: 25px;
-`;
-
-const LabelDesignShowBox = styled(CommonS.Center)`
-  width: 288px;
-  height: 100%;
-  border: 1px solid rgba(217, 219, 233, 1);
-  border-radius: 11px;
-`;
-
-const LabelDiv = styled.div<{ $backgroundColor: string; $textColor: string }>`
-  padding: 4px 16px;
-  border-radius: 16px;
-  background-color: ${(props) => props.$backgroundColor};
-  color: ${(props) => props.$textColor};
-  height: 28.5px;
-`;
-
-const CreateLabelTextArea = styled(CommonS.ColumnFlex)`
-  width: 904px;
-  height: 153px;
-  justify-content: space-between;
-`;
-
-const CreateLabelButtonArea = styled.div`
-  display: flex;
-  justify-content: flex-end;
-`;
-
-const CreateCancelButton = styled.button`
-  width: 128px;
-  height: 40px;
-  border-radius: 12px;
-  border: 1px solid rgba(0, 122, 255, 1);
-  background-color: white;
-  color: rgba(0, 122, 255, 1);
-  margin-right: 15px;
-`;
-
-const CreateDoneButton = styled.button`
-  background-color: rgba(0, 122, 255, 1);
-  border: none;
-  color: white;
-  width: 128px;
-  height: 40px;
-  border-radius: 12px;
-`;
-
-const InputContainer = styled.div<{ $width: string }>`
-  display: flex;
-  align-items: center;
-  width: ${(props) => props.$width};
-  height: 40px;
-  background-color: rgba(239, 240, 246, 1);
-  border-radius: 12px;
-  padding: 0 15px;
-
-  & label {
-    color: rgba(110, 113, 145, 1);
-    font-size: 12px;
-  }
-
-  & input {
-    border: none;
-    background-color: transparent;
-    width: 100%;
-  }
-`;
-
-const LabelBox = styled.div`
-  width: 64px;
-  display: flex;
-`;
-
-const ColorPickerBox = styled.div`
-  display: flex;
-  gap: 10px;
-`;
-
-const TextColorPicker = styled.select`
-  border: none;
 `;
