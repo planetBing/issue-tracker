84f2f485291176b3a3a6e8550302b6165412f82b
seondays <agseon9@gmail.com>
2024-05-28 15:23:29 +0900

    fix: 이슈의 열고닫음 여부 부분 수정 요청을 PATCH로 받도록 수정

--- be/src/main/java/issuetracker/be/controller/IssueController.java
+++ be/src/main/java/issuetracker/be/controller/IssueController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,12 +56,12 @@ public class IssueController {
     return issueService.getFilteredIssue(filterRequest);
   }
 
-  @PutMapping("/issue/open")
+  @PatchMapping("/issue/open")
   public void openIssues(@RequestBody OpenStatusChangeRequest openStatusChangeRequest) {
     issueService.changeIssueStatus(openStatusChangeRequest, true);
   }
 
-  @PutMapping("/issue/close")
+  @PatchMapping("/issue/close")
   public void closeIssue(@RequestBody OpenStatusChangeRequest openStatusChangeRequest) {
     issueService.changeIssueStatus(openStatusChangeRequest, false);
   }
