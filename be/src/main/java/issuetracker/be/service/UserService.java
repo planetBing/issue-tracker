f9c6776fc787b14ea1bc271a608a7570877f938f
wontae <dnjsxo0616@gmail.com>
2024-05-28 15:33:22 +0900

    feat: AssigneeRefRepository를 사용하여 특정 이슈의 담당자를 조회 기능 추가

--- be/src/main/java/issuetracker/be/service/UserService.java
+++ be/src/main/java/issuetracker/be/service/UserService.java
@@ -1,9 +1,13 @@
 package issuetracker.be.service;
 
+import issuetracker.be.domain.AssigneeRef;
 import issuetracker.be.domain.User;
+import issuetracker.be.repository.AssigneeRefRepository;
 import issuetracker.be.repository.UserRepository;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final AssigneeRefRepository assigneeRefRepository;
 
-  public UserService(UserRepository userRepository) {
+  @Autowired
+  public UserService(UserRepository userRepository, AssigneeRefRepository assigneeRefRepository) {
     this.userRepository = userRepository;
+    this.assigneeRefRepository = assigneeRefRepository;
   }
 
   public List<User> getAllUsers() {
@@ -24,5 +31,12 @@ public class UserService {
   public User getUser(String name) {
     return userRepository.findByName(name)
         .orElseThrow(() -> new NoSuchElementException("존재하지 않는 작성자입니다."));
+  }
+
+  public List<User> findByIssueId(Long issueId) {
+    List<AssigneeRef> allUser = assigneeRefRepository.findAllUser(issueId);
+    return allUser.stream()
+        .map(assigneeRef -> getUser(assigneeRef.getUser_name()))
+        .collect(Collectors.toList());
   }
 }
