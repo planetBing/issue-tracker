4b24804e87fa3754428683dfb501090eb2c7d5f1
seondays <agseon9@gmail.com>
2024-05-28 15:30:30 +0900

    feat: 예외 발생 시 응답을 주기 위한 예외처리 핸들러 구현

---
+++ be/src/main/java/issuetracker/be/exception/CustomExceptionHandler.java
@@ -0,0 +1,28 @@
+package issuetracker.be.exception;
+
+import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+  @ExceptionHandler(value = {MilestoneHasAssociatedIssuesException.class,
+      NoSuchElementException.class})
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ExceptionResponse handleBusinessLogicException(Exception e) {
+    log.error("{} : {}", e.getClass().getName(), e.getMessage());
+    return new ExceptionResponse(e.getMessage());
+  }
+
+  @ExceptionHandler(Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public ExceptionResponse handleGlobalException(Exception e) {
+    log.error("{} : {}", e.getClass().getName(), e.getMessage());
+    return new ExceptionResponse("예상하지 못한 오류입니다. 다시 시도해 주세요.");
+  }
+}
