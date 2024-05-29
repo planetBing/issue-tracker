2c4df96bf4e82d2bb86baf89ad47f393330d742a
wontae <dnjsxo0616@gmail.com>
2024-05-29 17:48:49 +0900

    refactor: 라벨 삭제 코드 수정

--- be/src/main/java/issuetracker/be/repository/LabelRepository.java
+++ be/src/main/java/issuetracker/be/repository/LabelRepository.java
@@ -2,7 +2,10 @@ package issuetracker.be.repository;
 
 import issuetracker.be.domain.Label;
 import java.util.List;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,4 +13,8 @@ public interface LabelRepository extends CrudRepository<Label, Long> {
 
   @Override
   List<Label> findAll();
+
+  @Modifying
+  @Query("DELETE FROM label WHERE id = :id")
+  void deleteLabel(@Param("id") Long id);
 }
