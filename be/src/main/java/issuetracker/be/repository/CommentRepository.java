4df2cfd3249f4f9f3b1f292fe70c3fa7c8b14ebf
wontae <dnjsxo0616@gmail.com>
2024-05-29 11:43:02 +0900

    feat: 이슈 삭제를 위한 comment의 issue_id 삭제 기능 구현 #43

--- be/src/main/java/issuetracker/be/repository/CommentRepository.java
+++ be/src/main/java/issuetracker/be/repository/CommentRepository.java
@@ -2,8 +2,10 @@ package issuetracker.be.repository;
 
 import issuetracker.be.domain.Comment;
 import java.util.List;
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,4 +15,8 @@ public interface CommentRepository extends CrudRepository<Comment, Long> {
   List<Comment> findByIssueId(Long id);
 
   List<Comment> findByReporter(String reporter);
+
+  @Modifying
+  @Query("DELETE FROM comment WHERE issue_id = :id")
+  void deleteIssue(@Param("id") Long id);
 }
