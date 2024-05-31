dd75159e5d57d0750cd06f6a3e1639b058de5c40
seondays <agseon9@gmail.com>
2024-05-31 10:49:32 +0900

    fix: 로그 전체 찍도록 임시변경

--- be/src/main/java/issuetracker/be/exception/CustomExceptionHandler.java
+++ be/src/main/java/issuetracker/be/exception/CustomExceptionHandler.java
@@ -15,14 +15,20 @@ public class CustomExceptionHandler {
       NoSuchElementException.class})
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ExceptionResponse handleBusinessLogicException(Exception e) {
-    log.error("ERROR : {}", e.getStackTrace()[0]);
+//    log.error("ERROR : {}", e.getStackTrace()[0]);
+    for(StackTraceElement i : e.getStackTrace()) {
+      System.out.println(i);
+    }
     return new ExceptionResponse(e.getMessage());
   }
 
   @ExceptionHandler(Exception.class)
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public ExceptionResponse handleGlobalException(Exception e) {
-    log.error("ERROR : {}", e.getStackTrace()[0]);
+//    log.error("ERROR : {}", e.getStackTrace()[0]);
+    for(StackTraceElement i : e.getStackTrace()) {
+      System.out.println(i);
+    }
     return new ExceptionResponse(e.getMessage() + " : 예상하지 못한 오류입니다. 다시 시도해 주세요.");
   }
 }
